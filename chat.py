e306261fbb05c33f142ab4cbf91d8e5bd28c0a2b
Harsh Rawat <harshrawat096@gmail.com>
2025-01-04 12:33:06 +0530

    Setup!

---
+++ chat.py
@@ -0,0 +1,10 @@
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
